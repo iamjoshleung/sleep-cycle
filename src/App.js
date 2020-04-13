5c8e7c51ad3b973e6c6d8266ec6d76cf8bafa93b
Joshua Leung <joshua_leung@sfu.ca>
2020-04-13 15:40:13 -0700

    initial app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import Upload from "./Upload";
+import Section from "./Section";
+import Score from "./Score";
+// import { CSSTransition } from "react-transition-group";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: "section",
+    };
+  }
+
+  handleData = (data) => {
+    this.setState({
+      currentView: "section",
+    });
+  };
+
+  handleViewChangeHome = () => {
+    this.setState({
+      currentView: "upload",
+    });
+  };
+
+  handleViewChangeScore = () => {
+    this.setState({
+      currentView: "score",
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <CSSTransition in={true} timeout={200} classNames="fade"> */}
+        <div>
+        {this.state.currentView == "upload" ? (
+              <Upload onFinishAnalyze={this.handleData} />
+            ) : null}
+            {this.state.currentView == "section" ? (
+              <Section
+                onHome={this.handleViewChangeHome}
+                onScore={this.handleViewChangeScore}
+              />
+            ) : null}
+            {this.state.currentView == "score" ? (
+              <Score onHome={this.handleViewChangeHome} />
+            ) : null}
+          </div>
+        {/* </CSSTransition> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/SignalGraph/SignalGraph.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SignalGraph.module.scss';
+
+const SignalGraph = () => (
+  <div className={styles.SignalGraph}>
+    SignalGraph Component
+  </div>
+);
+
+SignalGraph.propTypes = {};
+
+SignalGraph.defaultProps = {};
+
+export default SignalGraph;
